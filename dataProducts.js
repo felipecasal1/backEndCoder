9e9f03aa01b9b27c72ce2555fee19a6982acdcab
felipecasal1 <felipecasalcornejo@gmail.com>
2025-04-07 21:20:07 -0300

    actualizaciones de la primera pre entrega

---
+++ dataProducts.js
@@ -0,0 +1,47 @@
+
+
+
+
+const products = [
+    {
+        id:1,
+        titulo: "manzana",
+        price: 15,
+        stock:20,
+    },
+    {
+        id:2,
+        titulo: "pera",
+        price: 10,
+        stock:40,
+    },
+    {
+        id:3,
+        titulo: "mandarina",
+        price: 25,
+        stock:10,
+    },
+    {
+        id:4,
+        titulo: "palta",
+        price: 45,
+        stock:16,
+    },
+    {
+        id:5,
+        titulo: "naranja",
+        price: 15,
+        stock:30,
+    },
+    {
+        id:6,
+        titulo: "banana",
+        price: 25,
+        stock:50,
+    }
+    
+]
+
+
+
+export default products
---
+++ index.js
@@ -0,0 +1,12 @@
+const randomNum = (min, max) => Math.floor(Math.random()*max +1)
+
+for (let i = 0; i < 5; i++) {
+console.log(randomNum(1,20))
+}
+
+console.log("fin de la operacion")
+
+
+
+
+
